3913b8f337a815107e68bd5a9f1fb334906cf4fb
Nick Malcolm <nick@malcolm.net.nz>
2016-05-17 12:57:47 +1200

    Update tests

--- tests/Endpoint/EventsEndpointTest.php
+++ tests/Endpoint/EventsEndpointTest.php
@@ -20,11 +20,12 @@ class EventsEndpointTest extends \PHPUnit_Framework_TestCase
     public function testTrackLogIn()
     {
         $expected = [
-            'ip' => '127.0.0.1',
+            'ip' => '1.2.3.4',
             'user' => [
                 'id' => '86',
                 'name' => 'Maxwell Smart',
-                'email' => 'max@control.com'
+                'email' => 'max@control.com',
+                'mobile' => '+64270000001'
             ],
             'user_agent' => 'useragent',
             'endpoint' => 'events',
@@ -32,17 +33,18 @@ class EventsEndpointTest extends \PHPUnit_Framework_TestCase
             'verb' => 'log-in',
         ];
 
-        $this->assertTracking($expected, 'trackLogIn');
+        $this->assertTrackingWithHelper($expected, 'trackLogIn');
     }
 
     public function testTrackLogInDenied()
     {
         $expected = [
-            'ip' => '127.0.0.1',
+            'ip' => '1.2.3.4',
             'user' => [
                 'id' => '86',
                 'name' => 'Maxwell Smart',
-                'email' => 'max@control.com'
+                'email' => 'max@control.com',
+                'mobile' => '+64270000001'
             ],
             'user_agent' => 'useragent',
             'endpoint' => 'events',
@@ -50,10 +52,29 @@ class EventsEndpointTest extends \PHPUnit_Framework_TestCase
             'verb' => 'log-in-denied',
         ];
 
-        $this->assertTracking($expected, 'trackLogInDenied');
+        $this->assertTrackingWithHelper($expected, 'trackLogInDenied');
     }
 
-    protected function assertTracking($expected, $method)
+    public function testTrackEvent()
+    {
+        $expected = [
+            'ip' => '1.2.3.4',
+            'user' => [
+                'id' => '86',
+                'name' => 'Maxwell Smart',
+                'email' => 'max@control.com',
+                'mobile' => '+64270000001'
+            ],
+            'user_agent' => 'useragent',
+            'endpoint' => 'events',
+            'method' => 'POST',
+            'verb' => 'page-view',
+        ];
+
+        $this->assertTrack($expected);
+    }
+
+    protected function assertTrackingWithHelper($expected, $method)
     {
         $this->endpoint->expects($this->once())
             ->method('execute')
@@ -68,11 +89,36 @@ class EventsEndpointTest extends \PHPUnit_Framework_TestCase
                             && array_key_exists('id', $data['user']) && $data['user']['id'] == $expected['user']['id']
                             && array_key_exists('name', $data['user']) && $data['user']['name'] == $expected['user']['name']
                             && array_key_exists('email', $data['user']) && $data['user']['email'] == $expected['user']['email']
+                            && array_key_exists('mobile', $data['user']) && $data['user']['mobile'] == $expected['user']['mobile']
                         && array_key_exists('user_agent', $data) && $data['user_agent'] == $expected['user_agent']
                         ;
                 })
             );
 
         $this->endpoint->$method($expected['ip'], $expected['user'], $expected['user_agent']);
+    }
+
+    protected function assertTrack($expected)
+    {
+        $this->endpoint->expects($this->once())
+            ->method('execute')
+            ->with(
+                $expected['method'],
+                $expected['endpoint'],
+                $this->callback(function ($data) use ($expected) {
+                    return
+                        array_key_exists('verb', $data) && $data['verb'] == $expected['verb']
+                        && array_key_exists('ip', $data) && $data['ip'] == $expected['ip']
+                        && array_key_exists('user', $data)
+                            && array_key_exists('id', $data['user']) && $data['user']['id'] == $expected['user']['id']
+                            && array_key_exists('name', $data['user']) && $data['user']['name'] == $expected['user']['name']
+                            && array_key_exists('email', $data['user']) && $data['user']['email'] == $expected['user']['email']
+                            && array_key_exists('mobile', $data['user']) && $data['user']['mobile'] == $expected['user']['mobile']
+                        && array_key_exists('user_agent', $data) && $data['user_agent'] == $expected['user_agent']
+                        ;
+                })
+            );
+
+        $this->endpoint->trackEvent($expected['verb'], $expected['ip'], $expected['user'], $expected['user_agent']);
     }
 }
